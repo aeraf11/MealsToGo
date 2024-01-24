106c21fad3fbd2595d5958dbdce6dd23ebd2d84e
Aeraf Yousuf <aeraf11@gmail.com>
2024-01-24 12:06:41 +0000

    First commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Aeraf Yousuf, Hello World, It is good to see you!!</Text>
+      <Text>My name is Aeraf Yousuf, Hello World, It is good to see you!!!!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
